195d01ee93c5eb0728a13365e8d3cec472da4667
RAMDHAN-MQ <ramdhanqolbi@gmail.com>
2026-05-22 16:13:20 +0700

    fix: routing dan UI

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,46 @@
 @extends('layouts.master')
+
 @section('title', 'Dashboard Admin')
+
+@section('page-breadcrumb')
+<li class="breadcrumb-item text-muted">
+    Main Menu
+</li>
+<li class="breadcrumb-item fw-bold">
+    Dashboard
+</li>
+@endsection
+
 @section('content')
-hooh
+
+<h2 class="fw-bold">Selamat Datang, <span class="text-primary">{{ auth()->user()->name }}</span></h2>
+<div class="row my-4">
+    <div class="col-3">
+        <div class="card p-5 shadow-sm"></div>
+    </div>
+    <div class="col-3">
+        <div class="card p-5 shadow-sm"></div>
+    </div>
+    <div class="col-3">
+        <div class="card p-5 shadow-sm"></div>
+    </div>
+    <div class="col-3">
+        <div class="card p-5 shadow-sm"></div>
+    </div>
+</div>
+<div class="row my-4">
+    <div class="col-12">
+        <div class="card p-5 shadow-sm"></div>
+    </div>
+</div>
+<div class="row my-4">
+    <div class="col-6">
+        <div class="card p-5 shadow-sm"></div>
+    </div>
+    <div class="col-6">
+        <div class="card p-5 shadow-sm"></div>
+    </div>
+</div>
 
 @endsection
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\BalitaController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\IbuHamilController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,8 +18,25 @@ Route::controller(LoginController::class)->group(function () {
 
 // admin
 Route::prefix('admin')->middleware(['auth', 'role:admin'])->group(function () {
+    // dashboard
     Route::controller(DashboardController::class)->group(function () {
         Route::get('/dashboard', 'dashboard')->name('admin.dashboard');
+    });
+
+    // balita
+    Route::controller(BalitaController::class)->group(function () {
+        Route::get('/balita', 'index')->name('admin.balita.index');
+        Route::get('/balita/create', 'create')->name('admin.balita.create');
+        Route::post('/balita/store', 'store')->name('admin.balita.store');
+        Route::delete('/balita/destroy/{id}', 'destroy')->name('admin.balita.destroy');
+        Route::get('/balita/edit/{id}', 'edit')->name('admin.balita.edit');
+        Route::put('/balita/update/{id}', 'update')->name('admin.balita.update');
+        Route::get('/balita/view/{id}', 'view')->name('admin.balita.view');
+        });
+
+    // ibu hamil
+    Route::controller(IbuHamilController::class)->group(function () {
+        Route::get('/ibuhamil', 'index')->name('admin.ibu.index');
     });
 });
 
